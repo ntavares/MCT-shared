724c2e85269eaaa491d01c4a0866ab7211f588a2
root <root@bubble03.nl00.cs.leaseweb.net>
2016-01-11 18:01:53 +0000

    IGNORE just cosmetic output

--- deploy/postboot/post_detect_reboot.sh
+++ deploy/postboot/post_detect_reboot.sh
@@ -4,11 +4,11 @@
 NEWHOST=$1
 echo "Note: ${NEWHOST}: Waiting for the VM to boot..."
 # Wait until the VM is alive
-while ! ping -c1 ${NEWHOST} &>/dev/null; do :; done
+while ! ping -w 1 -c1 ${NEWHOST} &>/dev/null; do : echo -n . ; sleep 1; done
 echo "Note: ${NEWHOST}: Installing and configuring"
 echo "Note: ${NEWHOST}: This will take some time. You may send this to the background."
-while ping -c5 ${NEWHOST} &>/dev/null; do :; done
+while ping -w 1 -c5 ${NEWHOST} &>/dev/null; do : echo -n . ; sleep 1; done
 echo "Note: ${NEWHOST}: Rebooting"
-while ! ping -c1 ${NEWHOST} &>/dev/null; do :; done
+while ! ping -w 1 -c1 ${NEWHOST} &>/dev/null; do : echo -n . ; sleep 1; done
 sleep 15
 echo "Note: ${NEWHOST}: Ready for duty!"
